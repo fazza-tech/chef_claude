b22fa5eeeb39a94d7aa5376af32738c7629fd95e
fazza-tech <ebranfazza@gmail.com>
2024-12-24 21:40:49 +0530

    updated

---
+++ chef-claude/src/Header.jsx
@@ -0,0 +1,9 @@
+import logo from './images/chef-claude-icon.png'
+export default function Header(){
+    return(
+        <header>
+            <img src={logo} alt="logo" />
+            <h1>Chef Claude</h1>
+        </header>
+    )
+}
