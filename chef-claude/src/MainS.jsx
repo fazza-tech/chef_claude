65bca8b020c00141488d83c3f46b05558d0f53b3
fazza-tech <ebranfazza@gmail.com>
2024-12-27 07:51:50 +0530

    added useState

--- chef-claude/src/MainS.jsx
+++ chef-claude/src/MainS.jsx
@@ -1,17 +1,24 @@
+import React from "react"
+
 export default function MainS(){
-    const ingredients = ["Chicken","Oregano","Tomatoes"];
+    const [ingredients,stateIngredients ]=React.useState([])
 
     const ingredientsListItems = ingredients.map(ingredient => {
         return <li key={ingredient}>{ingredient}</li>
     })
-    function handleSubmit(){
-        console.log("submitted")
+    function handleSubmit(event){
+       event.preventDefault()
+       const formData = new FormData(event.currentTarget)
+       const newIngredient = formData.get("ingredient")
+       stateIngredients(prevIngredient => [...prevIngredient,newIngredient])
+
     }
     return(
         <main>
             <form onSubmit={handleSubmit} className="add-ingredient-form">
                 <input type="text"
                         placeholder="e.g. oregano"
+                        name="ingredient" 
                         aria-label="Add-Ingredient"        
                 />
                 <button > Add Ingredient</button>
